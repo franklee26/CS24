406f3486a1d63735d1e49587bae9abc5ec82bf27
Frank Lee <franklee@umail.ucsb.edu>
2017-11-02 14:19:26 -0700

    adding lab05

---
+++ lab05/intlist.cpp
@@ -0,0 +1,125 @@
+// intlist.cpp
+// Implements class IntList
+// Frank Lee, 2/11
+
+#include "intlist.h"
+
+#include <iostream>
+using std::cout;
+
+// return sum of values in list
+int IntList::sum() const {
+    int sum=0;
+    Node* copy=first;
+    while (copy!=NULL){
+        sum=sum+copy->info;
+        copy=copy->next;
+    }
+    return sum;
+}
+
+// returns true if value is in the list; false if not
+bool IntList::contains(int value) const {
+    Node* copy=first;
+    while (copy!=NULL){
+        if (copy->info==value){
+            return true;
+        } else {
+            copy=copy->next;
+        }
+    }
+    return false;
+}
+
+// returns maximum value in list, or 0 if empty list
+int IntList::max() const {
+    Node* copy=first;
+    int max=0;
+    while (copy!=NULL){
+        if (copy->info >= max){
+            max=copy->info;
+        }
+        copy=copy->next;
+    }
+    return max;
+    
+}
+
+// returns average (arithmetic mean) of all values, or
+// 0 if list is empty
+double IntList::average() const {
+    int count(0);
+    Node* copy=first;
+    while (copy!=NULL){
+        copy=copy->next;
+        count++;
+    }
+    if (count==0){
+        return 0;
+    }
+    return static_cast<double>(sum())/count;
+}
+
+// inserts value as new node at beginning of list
+void IntList::insertFirst(int value) {
+    Node* newhead=new Node;
+    newhead->info=value;
+    newhead->next=first;
+    first=newhead;
+}
+
+// DO NOT CHANGE ANYTHING BELOW (READ IT THOUGH)
+
+// constructor sets up empty list
+IntList::IntList() : first(0) { }
+
+// destructor deletes all nodes
+IntList::~IntList() {
+    Node *n = first;
+    while (n) {
+        Node *garbage = n;
+        n = n->next;
+        delete garbage;
+    }
+}
+
+// append value at end of list
+void IntList::append(int value) {
+    if (first == 0) { // empty list
+        first = new Node;
+        first->info = value;
+        first->next = 0;
+    }
+    else {
+        Node *n = first;
+        while (n->next) // not last node yet
+            n = n->next;
+        n->next = new Node;
+        n->next->info = value;
+        n->next->next = 0;
+    }
+}
+
+// print values enclose in [], separated by spaces
+void IntList::print() const {
+    Node *n = first;
+    cout << '[';
+    while (n) {
+        cout << n->info;
+        if (n->next)
+            cout << " ";
+        n = n->next;
+    }
+    cout << ']';
+}
+
+// return count of values
+int IntList::count() const {
+    int result = 0;
+    Node *n = first;
+    while (n) {
+        ++result;
+        n = n->next;
+    }
+    return result;
+}
